bbb152b0d9ba07ead7874876c31e5e0aa3056a01
Ayushi Verma <50084909+Ayushi20-19@users.noreply.github.com>
2021-12-21 15:52:15 +0530

    pokemon search app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import { useState, useEffect } from "react";
+import "./App.css";
+export default function App() {
+  const [dataItem, setDataItem] = useState([]);
+  useEffect(() => {
+    fetch("https://pokeapi.co/api/v2/pokemon?limit=100&offset=0")
+      .then((response) => response.json())
+      .then((data) => setDataItem(data.results));
+  }, []);
+  console.log(dataItem);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <input placeholder='search'></input>
+      <select name='type' id='type'>
+        <option value='type'>type</option>
+      </select>
+      {dataItem.map((item, index) => {
+        return (
+          <div value={index}>
+            <div className='CardMain'>
+              <div className='name'> {item.name}</div>
+              <div className='img-sec'>
+                <img
+                  src={
+                    "https://unpkg.com/pokeapi-sprites@2.0.2/sprites/pokemon/other/dream-world/" +
+                    index +
+                    ".svg"
+                  }></img>
+              </div>
+            </div>
+          </div>
+        );
+      })}
     </div>
   );
 }
-
-export default App;
